54c1381361df78352ed4c8d74f86ef81efad943d
Antonio Juliano <antonio.m.juliano@gmail.com>
2017-05-22 19:15:31 -0700

    Add social buttons (#7)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Col } from 'react-flexbox-grid';
 import Divider from 'material-ui/Divider';
+import Ionicon from 'react-ionicons';
 
 class Footer extends React.Component {
   constructor(props) {
@@ -23,7 +24,7 @@ class Footer extends React.Component {
       color: '#4c4c4c'
     }];
 
-    if (this.state.hovered !== null) {
+    if (this.state.hovered !== null && this.state.hovered < 3) {
       linkStyles[this.state.hovered].color = '#000000';
     }
 
@@ -69,7 +70,7 @@ class Footer extends React.Component {
                 {'Privacy Policy'}
               </Link>
             </div>
-            <div style={{ width: 'auto', margin: 'auto', marginLeft: 0, marginRight: 10 }}>
+            <div style={{ width: 'auto', margin: 'auto', marginLeft: 0, marginRight: 20 }}>
               <Link
                 to='/terms'
                 style={linkStyles[2]}
@@ -78,6 +79,36 @@ class Footer extends React.Component {
               >
                 {'Terms of Service'}
               </Link>
+            </div>
+            <div style={{ width: 'auto', margin: 'auto', marginRight: 5, marginLeft: 0, marginTop: 4 }}>
+              <a
+                href='https://twitter.com/weipointproject'
+                style={linkStyles[0]}
+                onMouseEnter={ () => this.setState({ hovered: 3 })}
+                onMouseLeave={ () => this.setState({ hovered: null })}
+                target='_blank'
+              >
+                <Ionicon
+                  icon="ion-social-twitter"
+                  fontSize="20px"
+                  color={ this.state.hovered === 3 ? '#000000' : "#4c4c4c"}
+                />
+              </a>
+            </div>
+            <div style={{ width: 'auto', margin: 'auto', marginRight: 0, marginLeft: 0, marginTop: 4 }}>
+              <a
+                href='https://www.facebook.com/weipoint'
+                style={linkStyles[0]}
+                onMouseEnter={ () => this.setState({ hovered: 4 })}
+                onMouseLeave={ () => this.setState({ hovered: null })}
+                target='_blank'
+              >
+                <Ionicon
+                  icon="ion-social-facebook"
+                  fontSize="20px"
+                  color={ this.state.hovered === 4 ? '#000000' : "#4c4c4c"}
+                />
+              </a>
             </div>
           </div>
         </Col>
